febd06d6fc60eb2497d5f96fdc65ad5a4a59b960
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-10 17:30:53 +0100

    Correct .to() to have no space after expect

--- greeter_spec.rb
+++ greeter_spec.rb
@@ -1,5 +1,23 @@
+# Tell RSpec where to find the production code we've written to pass the tests
+require './greeter'
+
+# 1. Set up the specification, trying to describe something.
 describe 'Greeter' do
+  # 2. Enclose the aspect of the greeter in an it block.
   it 'greets Rico' do
-    expect (greet('Rico')).to eq 'Hello, Rico, how are you today?'
+    # 3. Inside the it block add the test code - the actual expectation we're setting
+    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
   end
 end
+=begin
+# This is called Given, When, Then in parts.
+
+# Separating it into the Given, When, Then parts would be:
+it 'greets Rico' do
+  name = 'Rico' # given
+  result = greet(name) # When
+  expect(result).to eq 'Hello, Rico, how are you today?' # Then
+end
+
+# eq is a Matcher - essentially a method that will match the operation inside the expect method (result) with the expectation ('Hello, Rico, how are you today?')
+=end
